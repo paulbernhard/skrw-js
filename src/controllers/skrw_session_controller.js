8150253d8f46201e18806eb00b07b3407016908d
R4ttlesnake <mail@pbernhard.com>
2019-08-19 14:46:18 +0200

    reworked styles / session js controller

--- src/controllers/skrw_session_controller.js
+++ src/controllers/skrw_session_controller.js
@@ -2,22 +2,46 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
 
+  static targets = ["flash"]
+
   connect() {
-    if (this.loggedIn) {
-      this.element.classList.add(this.minClass)
+    if (this.hasFlashTarget) {
+      this.timeoutFlash()
     }
+
+    document.addEventListener("ajax:success", (event) => {
+      this.flash(event)
+    })
   }
 
   toggle() {
-    this.element.classList.toggle(this.minClass)
+    this.element.classList.toggle("skrw-session--pinned")
   }
 
-  get minClass() {
-    return "skrw-session--min"
+  // display flash messages from ajax / json responses
+  flash(event) {
+    const [response, status, xhr] = event.detail
+    console.log(response)
+
+    if ("flash" in response) {
+      console.log(response.flash)
+      let html = ""
+      Object.keys(response.flash).forEach(key => {
+        html += `<div class="${key}">${response.flash[key]}</div>`
+      })
+      this.flashTarget.innerHTML = html
+      this.timeoutFlash()
+    }
   }
 
-  get loggedIn() {
-    const loggedIn = this.data.get("loggedIn")
-    return loggedIn == "true" ? true : false
+  // hide flash after n seconds
+  timeoutFlash() {
+    const timer = setTimeout(() => {
+      this.flashTarget.innerHTML = ""
+    }, 2200)
   }
+
+  // get isExpanded() {
+  //   return this.element.classList.contains("skrw-session--expanded") ? true : false
+  // }
 }
